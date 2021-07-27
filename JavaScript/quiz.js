bdc204a77ae5dfab2ca542fd43cbf2f98ce0ccfe
Adhish Bahl <adhishbahl0@gmail.com>
2021-07-27 21:41:52 +0530

    modified for devanshi

--- JavaScript/quiz.js
+++ JavaScript/quiz.js
@@ -16,30 +16,39 @@ s35.style.display = "none";
 btn1.addEventListener("click", ()=> {
     btn1.style.display = "none";
     name.style.display = "block";
-    s35.style.display="block";
     setTimeout(()=>{
-        s35.style.display="none";
-        btn2.style.display="block";
-    }, 3500)
+        s35.style.display="block";
+        setTimeout(()=> {
+            s35.style.display="none";
+            btn2.style.display="block";
+        },10690);
+    }, 10000)
 })
 
 btn2.addEventListener("click", ()=> {
     btn2.style.display = "none";
-    s35.textContent = "Oops, something went wrong again :c , wait for 5.7 seconds please! This is last time Pakaa!!";
+    name.textContent
+    s35.textContent = "It was your brain which went missing. Here 🧠 we got it for you :)";
     s35.style.display = "block";
     setTimeout(()=> {
-        s35.style.display = "none";
-        btn3.style.display = "block";
-    }, 5700)
+        s35.textContent = "Ruko ruko, abhi itne jaldi thode jana hai ! Wait for 6.9s  ";
+        setTimeout(()=> {
+            s35.style.display = "none";
+            btn3.style.display = "block";
+        }, 6900)
+    }, 8000)
 })
 
 btn3.addEventListener("click", ()=> {
     btn3.style.display = "none";
-    s35.textContent = "Kindly wait, your system is slow! we are loading the messasage! Approx time remaining: 10seconds...";
+    s35.textContent = "Wait your system is slow! We are loading the messasage! Approx time remaining: 69.69 seconds...";
     s35.style.display = "block";
     setTimeout(()=> {
-        s35.style.display = "none";
-        coffee.style.display = "block";
-        btn4.style.display = "none";
-    }, 10000)
+        s35.textContent = "We used Adhish's power to load it faster! Your system is trash, just like you are!";
+        setTimeout(()=>{
+            s35.style.display = "none";
+            coffee.style.display = "block";
+            btn4.style.display = "none";
+        }, 8000)
+    }, 11000)
 })
