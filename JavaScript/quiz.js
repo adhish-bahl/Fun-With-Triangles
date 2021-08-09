ef03146e0390ef5884de597636c10ba5a8954c73
Adhish Bahl <adhishbahl0@gmail.com>
2021-08-10 00:04:16 +0530

    disabling submit button after its press

--- JavaScript/quiz.js
+++ JavaScript/quiz.js
@@ -1,12 +1,13 @@
 const check = document.querySelector(".submit");
 const correctAns = document.querySelectorAll(".correctAns");
 const output = document.querySelector(".output");
-let score = 0;
 
 check.addEventListener("click", ()=> {
+    let score = 0;
     for(let i = 0; i < correctAns.length; i++) {
         if(correctAns[i].checked == true)
             score++;
     }
     output.textContent = "You scored " + score + "/10 points.    ";
+    check.style.display = "none";
 })
